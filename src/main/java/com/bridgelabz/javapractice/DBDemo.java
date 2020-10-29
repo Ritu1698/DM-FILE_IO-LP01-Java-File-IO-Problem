ae2c0b36472f1b4008bb66d011a4c1fd986a94c4
Ritu1698 <biswasrituparna83@gmail.com>
2020-10-29 10:55:10 +0530

    [Rituparna]Added: Method to set DB Connection using DBDemo Class

---
+++ src/main/java/com/bridgelabz/javapractice/DBDemo.java
@@ -0,0 +1,38 @@
+package com.bridgelabz.javapractice;
+
+import java.sql.Connection;
+import java.sql.Driver;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+
+public class DBDemo {
+    public static void main(String [] args){
+        String jdbcURL = "jdbc:mysql://localhost:3306/payroll_service?allowPublicKeyRetrieval=true&useSSL=false";
+        String userName = "root";
+        String password = "root";
+        Connection con;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("System loaded");
+        }
+        catch (ClassNotFoundException ignored){
+
+        }
+        listDrivers();
+        try {
+            System.out.println("Connecting to database:" + jdbcURL);
+            con = DriverManager.getConnection(jdbcURL,userName,password);
+            System.out.println("Connection is successful!!!!!"+con);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public static void listDrivers(){
+        Enumeration<Driver> drivers = DriverManager.getDrivers();
+        while (drivers.hasMoreElements()){
+            Driver driverClass = drivers.nextElement();
+            System.out.println("    "+driverClass.getClass().getName());
+        }
+    }
+}
